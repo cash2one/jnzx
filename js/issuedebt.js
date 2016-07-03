ab7c95b8336f4683c0f16da8081811542e72e46a
polojacky <yang.liucst@gmail.com>
2016-07-03 23:07:55 +0800

    新增发布债务页面
    修改了部分链接，新增发布债务界面

---
+++ js/issuedebt.js
@@ -0,0 +1,33 @@
+/**
+ * Created by jack on 16/6/30.
+ */
+
+$(document).ready(function () {
+    /*update nav bar*/
+    updateNav();
+
+    /*time picker: http://www.bootcss.com/p/bootstrap-datetimepicker/demo.htm */
+    $(".form_datetime").datetimepicker({
+        format: "dd MM yyyy - hh:ii"
+    });
+
+
+});
+
+//update nav bar based on current url address
+function updateNav() {
+    var position = window.location.pathname;
+    var dot1 = position.indexOf('.')
+    if (dot1 < 0) {  //home
+        $('#index').addClass('active');
+    } else {
+        //index.html or other
+        var slash1 = position.lastIndexOf('/');
+        if (slash1 < 0) {  // impossible
+            $('#index').addClass('active');
+        } else {
+            var posName = position.substring(slash1 + 1, dot1);
+            $('#' + posName).addClass('active');
+        }
+    }
+}
